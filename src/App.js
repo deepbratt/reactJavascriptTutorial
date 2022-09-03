f53efd5ec2756c4e6ea6515bf7fa5afd469368d5
Deep Bratt Mondal <deepbratt@gmail.com>
2022-09-03 22:54:58 +0530

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { Container } from "react-bootstrap";
+import "./app.css";
 import AreaDynamic from "./Component/AreaDynamic";
 import AreaOfCircle from "./Component/AreaOfCircle";
 import AreaOfReactangle from "./Component/AreaOfReactangle";
+import Grid from "./Component/Grid";
+import Header from "./Component/Header";
 function App() {
   return (
-    <div className="App">
-      <AreaOfReactangle />
-      <AreaDynamic />
-      <AreaOfCircle />
+    <div className="amarContainer">
+      <Header />
+      <Container>
+        {/* <Row>
+          <Col></Col>
+          <Col>2 of 2</Col>
+        </Row>
+
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row> */}
+        <Grid />
+        <AreaOfReactangle />
+        <AreaDynamic />
+        <AreaOfCircle />
+      </Container>
     </div>
   );
 }
